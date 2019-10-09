b12965839d5e979edb4283420809946414f409a3
Thomas RIPPOL <thomas.rippol@etu.univ-amu.fr>
2019-10-09 16:26:51 +0200

    javascript

--- showmenu.js
+++ showmenu.js
@@ -1,17 +1,15 @@
 function showmenu() {
     var divcon = document.getElementById('divconnexion');
     var divinsc = document.getElementById('divinscription');
-    if (divinsc.style.visibility == 'hidden' && divcon.style.visibility == 'hidden') {
-        divinsc.style.visibility = 'visible';
+    if (divinsc.style.visibility == 'hidden') {
+
         divcon.style.visibility = 'hidden';
+        divinsc.style.visibility = 'visible';
     }
-    else if (divcon.style.visibility == 'hidden' && divinsc.style.visibility == 'hidden')
+    else if (divcon.style.visibility == 'hidden')
     {
+        divinsc.style.visibility = 'hidden';
         divcon.style.visibility = 'visible';
-        divinsc.style.visibility = 'hidden';
     }
-    else {
-        divcon.style.visibility = 'hidden'
-        divinsc.style.visibility = 'hidden';
-    }
+
 }
