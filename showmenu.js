a79c9e3766461bb0c3cac58411c79f56d6414d1a
Thomas RIPPOL <thomas.rippol@etu.univ-amu.fr>
2019-10-09 16:37:59 +0200

    javascript

--- showmenu.js
+++ showmenu.js
@@ -8,7 +8,7 @@ function showmenu() {
         divcon.style.visibility = 'visible';
         divinsc.style.visibility = 'hidden';
     }
-    else if (divcon.style.visibility == 'hidden')
+    else
     {
         divinsc.style.visibility = 'visible';
         divcon.style.visibility = 'hidden';
