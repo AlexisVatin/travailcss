6cd4c84e9436c363bdd51d8b88ac3437bbb8ef52
r18002537 <thomas.rippol@etu.univ-amu.fr>
2019-10-17 11:41:34 +0200

    Correction du css

--- index.php
+++ index.php
@@ -4,36 +4,29 @@
 
 
 <div id="divconnexion" style="visibility: hidden">
-    <h3> Connexion </h3><br>
+    <h3> Connexion </h3>
     <form method="" id="form" class="formIC" action="">
-        <label>Nom utilisateur :</label><br>
+        <label>Nom utilisateur :</label>
         <input type="text" class="inputform" name="username">
-        <br><br>
-        <label>Mot de passe :</label><br>
+        <label>Mot de passe :</label>
         <input type="password" class="inputform" name="password">
-        <br><br>
         <a href=""> Mot de passe oublié ? </a>
-        <br><br>
         <input type="Submit" class="boutonform" value="Validation">
 
     </form>
 </div>
 
 <div id="divinscription" style="visibility: hidden">
-    <h3> Inscription </h3><br>
+    <h3> Inscription </h3>
     <form method="" id="form" class="formIC" action="">
-        <label> Pseudo : </label><br>
+        <label> Pseudo : </label>
         <input type="text" class="inputform" name="username">
-        <br><br>
-        <label> Mot de passe : </label><br>
+        <label> Mot de passe : </label>
         <input type="password" class="inputform" name="password">
-        <br><br>
-        <label> Vérification mot de passe : </label><br>
+        <label> Vérification mot de passe : </label>
         <input type="password" class="inputform" name="password">
-        <br><br>
-        <label> Adresse mail : </label><br>
+        <label> Adresse mail : </label>
         <input type="text" class="inputform" name="mail">
-        <br><br>
         <input type="Submit" class="boutonform" value="Validation">
 
     </form>
