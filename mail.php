de6ba00d039b47a926b129c76444f13c3b863f85
r18002537 <thomas.rippol@etu.univ-amu.fr>
2019-10-17 09:05:32 +0200

    mail.php

--- mail.php
+++ mail.php
@@ -3,9 +3,11 @@
     $probleme = $_GET['probleme'];
     $email = $_GET['email'];
     $commentaire = $_GET['commentaire'];
+    $commentaire = $commentaire . PHP_EOL . 'De :' . $email;
     $to = 'thomas.rippol@etu.univ-amu.fr';
 
-    mail($to,$probleme,$commentaire)
+    mail($to,$probleme,$commentaire);
 
+    header('location: index.php')
 
 ?>
